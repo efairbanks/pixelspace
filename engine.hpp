cb74d28acee03dce15e7e546f818be0254b85043
Eric Fairbanks <efairbanks@grasshopper.com>
2014-02-28 20:45:24 -0500

    Addeddddddddd engine.hpp

---
+++ engine.hpp
@@ -0,0 +1,90 @@
+#ifndef PIXELSPACE_ENGINE_H
+#define PIXELSPACE_ENGINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <list>
+#include "SDL/SDL.h"
+
+class SpaceObject {
+public:
+  double x;
+  double y;
+  double xAccel;
+  double yAccel;
+  Uint32 ticks;
+  
+  SpaceObject(double x, double y, double xAccel, double yAccel);
+  Uint32 Tick();
+  void Render();
+};
+
+class VolatilePixel : public SpaceObject {
+public:
+  Uint8 r;
+  Uint8 g;
+  Uint8 b;
+  
+  VolatilePixel(double x, double y,
+		double xAccel, double yAccel,
+		Uint8 r, Uint8 g, Uint8 b);
+  void Render();
+  void Tick();
+};
+
+class Player {
+public:
+  Player(SDLKey rotateLeft,
+	 SDLKey rotateRight,
+	 SDLKey boost,
+	 SDLKey fire);
+  Uint32 Tick();
+  void SetInput(SDLKey key, bool keyDown);
+  // -- //
+  SDLKey rotateLeft;
+  SDLKey rotateRight;
+  SDLKey boost;
+  SDLKey fire;
+  // -- //
+  double angle;
+  double magnitude;
+  // -- //
+  SpaceObject* ship;
+};
+
+class PixelSpace {
+public:
+  static bool running;
+  static Uint32 ticks;
+  std::list<SpaceObject*> spaceObjects;
+  std::list<Player*> players;
+  // --- //
+  static PixelSpace* Engine();
+  static PixelSpace* Engine(unsigned int screenWidth,
+			    unsigned int screenHeight,
+			    unsigned int frameRate);
+  void DrawPixel(double x, double y,
+		 Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+  void DrawSurface(double x, double y,
+		   SDL_Surface* surface);
+  void FillScreen(Uint8 r, Uint8 g, Uint8 b, Uint8 a);
+  Uint32 Tick();
+  bool ShutDown();
+  // --- //
+  static PixelSpace* _engine;
+  SDL_Surface* _screen;
+  unsigned int _screenWidth;
+  unsigned int _screenHeight;
+  unsigned int _frameRate;
+  // --- //
+  static Uint32 _FrameCallback(Uint32 interval,
+			       void* param);
+  // --- //
+  PixelSpace(){};
+  PixelSpace(PixelSpace const&){};
+  PixelSpace& operator=(PixelSpace const&){};
+};
+
+#endif
